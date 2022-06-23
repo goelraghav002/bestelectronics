8656fa23f78c18322a556192d431e4cc8b425f22
Raghav Goel <goelraghav002@gmail.com>
2022-06-23 11:58:26 +0530

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Head from "next/head";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Best Online Audio Products Store</title>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8959156253229201"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
